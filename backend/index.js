3c901c37debcc57c88817f43fc8b23bbe232d7d1
velan <velanv611@gmail.com>
2024-10-13 12:45:33 +0530

    cors add in index.js

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const path = require('path');
-const mongo_db = require('./config/mongo_db')
+const mongo_db = require('./config/mongo_db');
+const cors = require('cors');
 
 dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
 
+app.use(cors());
 app.use(express.json());
 
 mongo_db();
-
 
 app.use('/api', require('./src/routes'))
 

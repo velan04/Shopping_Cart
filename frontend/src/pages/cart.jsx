189e9fdb9383b66f37f376c377a042b3ed13fc51
velan <velanv611@gmail.com>
2024-11-19 11:58:08 +0530

    cart qty and order summary added

--- frontend/src/pages/cart.jsx
+++ frontend/src/pages/cart.jsx
@@ -1,35 +1,53 @@
 import React from 'react';
 import { Fragment } from 'react';
 
-const Cart = (cartItems, setCartItems) => {
-    const [count, setCount] = React.useState(1);
-    const handleIncrement = () => {
-        setCount(count + 1);
-    };
-    const handleDecrement = () => {
-        if (count > 1) {
-            setCount(count - 1);
+const Cart = ({cartItems, setCartItems}) => {
+    function increaseQty (item) {
+        if(item.product.stock == item.count){
+            return;
         }
-    };
-    const handleChange = (event) => {
-        setCount(parseInt(event.target.value));
-    };
+        const updatedItems = cartItems.map((i) => {
+            if(i.product._id == item.product._id){
+                i.count + 1
+            }
+            return i;
+
+        })
+        setCartItems(updatedItems);
+    }
+
+    function decreaseQty (item) {
+        if(item.count == 1){
+            return;
+        }
+        const updatedItems = cartItems.map((i) => {
+            if(i.product._id == item.product._id){
+                i.count - 1
+            }
+            return i;
+
+        })
+        setCartItems(updatedItems);
+    }
     
-    const handleRemove = () => {
-        setCartItems(cartItems.filter((item) => item.product.id !== cartItems.product.id));
-    };
+   function removeItem(item) {
+    const updatedCartItems = cartItems.filter((i) => {
+        if(i.product._id !== item.product._id){
+
+        };
+   })
+   setCartItems(updatedCartItems);
+}
 
     
 
     console.log(cartItems);
   return (<>
-      <div className='mb-24' key={cartItems.cartItems.product}>
-          <h1 className='text-4xl border-b-2 m-9 pb-4 border-gray-300'>Shopping Cart</h1>
-
+      <div className='mb-24' key={cartItems.product}>
           <div className='flex flex-col gap-6'>
-            
+              <h1 className='text-3xl border-b-2 m-9 pb-4 border-gray-300'>Your Cart - {cartItems.length} items </h1>
+              {cartItems.map((item) => (
               <div className='grid grid-cols-4 mx-3 items-center'>
-                {cartItems.cartItems.map((item) => (
                     <Fragment>
                         <img src={item.product.img} alt='products'
                             width={300} height={200} className='rounded-md' />
@@ -38,30 +56,51 @@ const Cart = (cartItems, setCartItems) => {
                             <div>
                                 <div className='flex gap-2 my-3'>
                                     <button className='text-white bg-slate-900 size-8 rounded'
-                                        onClick={handleDecrement}>
+                                          onClick={() => decreaseQty(item)}>
                                         -
                                     </button>
-                                    <input type="number" value={count} onChange={handleChange} readOnly
+                                    <input type="number" value={item.count} readOnly
                                         className='w-10 text-right border border-gray-300' />
                                     <button className='text-white bg-slate-900 size-8 rounded'
-                                        onClick={handleIncrement}>
+                                          onClick={() => increaseQty(item)}>
                                         +
                                     </button>
                                     <button className='text-white bg-slate-900 size-8 rounded w-28 ml-9'
-                                        onClick={handleRemove}>Remove
+                                          onClick={() => removeItem(item)}>Remove
                                     </button>
                                 </div>                           
                                  </div>
                         </div>
-                        <div className='font-extrabold mt-1 col-start-4'>
+                        <div className='font-extrabold mt-1 text-center col-start-3'>
                             <h3>Price</h3>
                             <h3 className='text-xl'>{item.product.price}</h3>
                         </div>
                     </Fragment>
-                    ))}
                 </div>
-              <hr />
+              ))}
           </div>
+          {
+            cartItems.map((item) => (
+          <div className='flex flex-col justify-center gap-6 px-4 w-72 h-full
+            fixed right-2 top-0 bg-white border-l-2'>
+              <h1 className='text-2xl text-center font-semibold'>Order Summary</h1>
+              <div className='flex justify-between'>
+                  <h3>Subtotal</h3>
+                  <h3>{item.product.count * item.product.price}</h3>
+              </div>
+              <div className='flex justify-between'>
+                  <h3>Shipping</h3>
+                  <h3>Rs. 100</h3>
+              </div>
+              <div className='flex justify-between font-medium text-xl'>
+                  <h3 className=''>Total</h3>
+                  <h3>Rs. 1100</h3>
+              </div>
+              <button>
+                  <h3 className='mt-3 text-white bg-slate-900 text-center p-2 rounded'>Proceed to Checkout</h3>
+              </button>
+          </div>
+          ))}
       </div></>
   )
 }

1720120b6a8408bd64bab50dc0999a02dd2808d7
velan <velanv611@gmail.com>
2024-11-24 20:00:33 +0530

    stock reducded method added

--- backend/src/controllers/order_controller.js
+++ backend/src/controllers/order_controller.js
@@ -1,13 +1,21 @@
 const orderModel = require('../models/order_model');
+const productModel = require('../models/product_model');
 
 //create Order - /api/orders
 const createOrder = async (req, res, next) => {
     const cartItems = req.body;
-    const amount = cartItems.reduce((acc, item) => (acc + item.product.price * item.qty), 0).toFixed(2); 
+    const amount =Number (cartItems.reduce((acc, item) => (acc + item.product.price * item.count), 0).toFixed(2)); 
+    console.log(amount)
     const status = 'pending';
-    
     const order = await orderModel.create({cartItems, amount, status})
-    console.log(order);
+
+    //updating product stock
+    cartItems.forEach(async (item) => {
+        const product = await productModel.findById(item.product._id);
+        product.stock -= item.count;
+        await product.save();
+    })
+
     res.json({
         success:true,
         order
--- backend/src/models/product_model.js
+++ backend/src/models/product_model.js
@@ -11,6 +11,8 @@ const productSchema = new mongoose.Schema({
     ],
     createdAt: Date,
     review: Number,
+    category: String,
+    stock: Number,
     qty: Number,
 })
 
